cee2fcf8f03d24822d68543ed99f69d5cd930c50
David-Estevez <david.estevez.fdez@gmail.com>
2015-03-26 14:40:29 +0100

    Add ouline generator script

---
+++ utils/kicad_board.py
@@ -0,0 +1,78 @@
+
+class KicadBoard:
+    def __init__(self, file_path):
+        # Read kicad_pcb file
+        self.file_path = file_path
+
+        with open(file_path, 'r') as f:
+            self.board_raw = f.readlines()
+
+
+    def add_rect_outline(self, width, height, corner_round_radius = 0, line_width = 0.2):
+        outline = []
+
+        # Generate corners:
+        corners = [ [0,0], [width, 0], [width, height], [0, height]]
+
+        if corner_round_radius > 0:
+            # Do outline with curved corners
+            # Generate straight segments:
+            origins = [point[:] for point in corners]
+            origins[0][0] += corner_round_radius
+            origins[1][1] += corner_round_radius
+            origins[2][0] -= corner_round_radius
+            origins[3][1] -= corner_round_radius
+
+            ends = [point[:] for point in corners]
+            ends.append(ends.pop(0)) # Shift list by 1
+            ends[0][0] -= corner_round_radius
+            ends[1][1] -= corner_round_radius
+            ends[2][0] += corner_round_radius
+            ends[3][1] += corner_round_radius
+
+            for origin, end in zip(origins, ends):
+                outline.append('(gr_line (start %f %f) (end %f %f) (angle 90) (layer Edge.Cuts) (width %f))' % ( origin[0], origin[1], end[0], end[1], line_width))
+
+            # Generate arcs
+            arc_origins = [point[:] for point in ends]
+            arc_origins[0][1] += corner_round_radius
+            arc_origins[1][0] -= corner_round_radius
+            arc_origins[2][1] -= corner_round_radius
+            arc_origins[3][0] += corner_round_radius
+
+            arc_start = [point[:] for point in ends]
+
+            for origin, end in zip(arc_origins, arc_start):
+                outline.append('(gr_arc (start %f %f) (end %f %f) (angle 90) (layer Edge.Cuts) (width %f))' % ( origin[0], origin[1], end[0], end[1], line_width))
+
+        else:
+            for i in range(len(corners)):
+                origin = corners[i]
+                end = corners[(i+1)%len(corners)]
+                outline.append('(gr_line (start %f %f) (end %f %f) (angle 90) (layer Edge.Cuts) (width %f))' % ( origin[0], origin[1], end[0], end[1], line_width))
+
+        # Add outline to the end of the file:
+        self.board_raw = self.board_raw[:-1]
+        for line in outline:
+            self.board_raw.append('  '+line+'\n')
+        self.board_raw.append(')\n')
+
+        print self.board_raw
+
+        # Return result
+        return outline
+
+    def save(self, out_file = None):
+        if not out_file:
+            out_file = self.file_path
+
+        with open(out_file, 'w') as f:
+                f.writelines(self.board_raw)
+
+if __name__ == '__main__':
+    board = KicadBoard('../../etch-a-sketch.kicad_pcb')
+    for line in board.add_rect_outline(50, 50, 5, 0.2):
+        print line
+
+
+    board.save()
